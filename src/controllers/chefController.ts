3065d05fbd16a633a509120c14978ed0359de351
Ashish Kumar <ashish380431@gmail.com>
2024-09-15 22:27:48 +0530

    health route and controller added

--- src/controllers/chefController.ts
+++ src/controllers/chefController.ts
@@ -24,3 +24,13 @@ export const calculateOrderEstimationTime = async (req: Request, res: Response) 
     return res.status(500).json({ error: `Error calculating estimation time for order ${id}.` });
   }
 };
+
+export const checkHealth = async (req: Request, res: Response) => {
+  try {
+    res.status(200).json({
+      "message":"chef service working fine"
+    });
+  } catch (error) {
+    res.status(500).json({ error: "chef service not working" });
+  }
+};
--- src/routes/chefRoutes.ts
+++ src/routes/chefRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { calculateOrderEstimationTime } from '../controllers/chefController';
+import { calculateOrderEstimationTime, checkHealth } from '../controllers/chefController';
 
 const router = Router();
 
 // Route to calculate estimated time for an order
 router.get('/estimate-time', calculateOrderEstimationTime);
+
+// check health
+router.get('/health', checkHealth);
 
 export default router;
